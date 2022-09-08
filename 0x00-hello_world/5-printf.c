89e69fe3b53bc5ed2cf0002516ee91b9ab307c34
cloobTech <belkid98@gmail.com>
2022-09-08 05:56:06 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	/*
+	 * print hello world
+	 */
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
