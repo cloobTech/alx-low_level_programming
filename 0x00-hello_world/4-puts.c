002fd3be66834299b86b0394cd5819b386cb5728
cloobTech <belkid98@gmail.com>
2022-09-08 05:39:21 -0700

    executable

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	/*
+	 * print hello world
+	 */
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
