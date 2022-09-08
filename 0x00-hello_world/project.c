3ecbe24cc86e279e094aad01ec4b1d2f6a7688d4
cloobTech <belkid98@gmail.com>
2022-09-08 04:40:27 -0700

    update

---
+++ 0x00-hello_world/project.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	/*
+	 * print hello world
+	 */
+	printf("Hello, World");
+	return (0);
+}
