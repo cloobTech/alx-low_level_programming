a84a70e991cc12574476a9fe4bd90905cb276d22
cloobTech <belkid98@gmail.com>
2022-09-09 07:21:20 -0700

    No

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		if (a == 'q'|| a == 'e')
+			continue;
+		else
+			putchar(a);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
