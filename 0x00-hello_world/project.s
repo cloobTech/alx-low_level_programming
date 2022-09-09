afd27f6e398e7c30024fb4ef4426972f1d9b0468
cloobTech <belkid98@gmail.com>
2022-09-09 00:21:39 -0700

    m

--- 0x00-hello_world/project.s
+++ 0x00-hello_world/project.s
@@ -1,4 +1,5 @@
 	.file	"project.c"
+	.intel_syntax noprefix
 	.text
 	.section	.rodata
 .LC0:
@@ -10,16 +11,16 @@ main:
 .LFB0:
 	.cfi_startproc
 	endbr64
-	pushq	%rbp
+	push	rbp
 	.cfi_def_cfa_offset 16
 	.cfi_offset 6, -16
-	movq	%rsp, %rbp
+	mov	rbp, rsp
 	.cfi_def_cfa_register 6
-	leaq	.LC0(%rip), %rdi
-	movl	$0, %eax
+	lea	rdi, .LC0[rip]
+	mov	eax, 0
 	call	printf@PLT
-	movl	$0, %eax
-	popq	%rbp
+	mov	eax, 0
+	pop	rbp
 	.cfi_def_cfa 7, 8
 	ret
 	.cfi_endproc
