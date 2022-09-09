fde72f5e9f6cd85341d2432851d5960228187f47
cloobTech <belkid98@gmail.com>
2022-09-09 09:56:04 -0700

    no

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int digit1, digit2;
+
+	for (digit1 = 0; digit1 <= 9; digit1++)
+		for (digit2 = 0; digit2 <= 9; digit2++)
+		{
+			if (digit1 == digit2)
+				continue;
+			putchar(digit1 + '0');
+			putchar(digit2 + '0');
+			if (digit1 == 9 && digit2 == 8)
+				break;
+			putchar(',');
+			putchar(' ');
+		}
+
+	putchar('\n');
+
+	return (0);
+}
