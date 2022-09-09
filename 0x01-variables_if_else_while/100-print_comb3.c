dcdd9176ec3726c7c3f7148a082bfe15642505cc
cloobTech <belkid98@gmail.com>
2022-09-09 10:30:21 -0700

    no

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,11 +13,11 @@ int main(void)
 	for (digit1 = 0; digit1 <= 9; digit1++)
 		for (digit2 = 0; digit2 <= 9; digit2++)
 		{
-			if (digit1 == digit2)
+			if (digit1 == digit2 || digit2 < digit1)
 				continue;
 			putchar(digit1 + '0');
 			putchar(digit2 + '0');
-			if (digit1 == 9 && digit2 == 8)
+			if (digit1 == 8 && digit2 == 9)
 				break;
 			putchar(',');
 			putchar(' ');
