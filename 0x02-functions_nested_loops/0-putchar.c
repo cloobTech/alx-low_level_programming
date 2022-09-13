7f2ab87f50c774790bbd6c3349d8a30b113a8203
cloobTech <belkid98@gmail.com>
2022-09-13 01:41:10 -0700

    print

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include "_putchar.c"
+
+/**
+ * main - Entry Point
+ * Desc - Print "_putchar" to the console
+ * Return: always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	_putchar(95);
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
