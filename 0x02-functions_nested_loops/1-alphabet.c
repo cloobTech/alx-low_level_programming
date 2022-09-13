2ab57fa30081fec7982d6f9961535fad36e10dbd
cloobTech <belkid98@gmail.com>
2022-09-13 02:07:23 -0700

    print

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print letter 'a - z'
+ *
+ */
+
+void print_alphabet(void)
+{
+	int a;
+
+	for (a = 'a'; a <= 'z'; a++)
+		_putchar(a);
+	_putchar('\n');
+}
