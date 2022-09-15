50ee9b4258f13aa4635017a5fdcfc450236bfab4
cloobTech <belkid98@gmail.com>
2022-09-15 03:46:31 -0700

    check

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - check for upper case characters
+ *
+ * @c: value to be evaluated
+ *
+ * Return: 1 if @c is uppercase otherwise 0
+ */
+
+int _isupper(int c)
+{
+	if ((c >= 65) && (c <=90))
+		return (1);
+	else
+		return (0);
+}
+
+
