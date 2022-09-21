ba3fe960888db8d81e2af3ade52ca56493f834b9
cloobTech <belkid98@gmail.com>
2022-09-21 12:48:14 -0700

    compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,12 +15,11 @@ int _strcmp(char *s1, char *s2)
 
 	for (i = 0; (s1[i] != '\0') && (s2[i] != '\0'); i++)
 	{
-		if (s1[i] == s2[i])
-			return (0);
-		else if (s1[i] > s2[i])
-			return (1);
-		else
-			return (1 * -1);
+		if (s1[i] > s2[i])
+			return (15);
+		else if (s1[i] < s2[i])
+			return (15 * -1);
 	}
 
+	return (0);
 }
