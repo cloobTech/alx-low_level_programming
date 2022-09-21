28039361f092b10a34092e02cb2a8f069234996b
cloobTech <belkid98@gmail.com>
2022-09-21 11:54:39 -0700

    compare

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings.
+ *
+ * @s1: first operand
+ * @s2: second operand
+ *
+ * Return: 0 if s1 == s2 else 15 or -15 when an equal check is met..
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; (s1[i] != '\0') && (s2[i] != '\0'); i++)
+	{
+		if (s1[i] == s2[i])
+			return (0);
+		else if (s1[i] > s2[i])
+			return (15);
+		else
+			return (15 * -1);
+	}
+
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,4 +6,5 @@ int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncpy(char *dest, char *src, int n);
 int _strlen(char *s);
+int _strcmp(char *s1, char *s2);
 #endif /*MAIN_H*/
