ca656b8706a04f04b50cacab9af1227cf8d4ee7e
cloobTech <belkid98@gmail.com>
2022-09-22 02:54:31 -0700

    capitalize

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string.
+ * @str: string to be operated on
+ * Return: operated @str
+ */
+
+char *cap_string(char *str)
+{
+	int i, ii, len;
+
+	char seperators[] = {9, 10, 32, 44, 46, 59, 33, 34, 63, 40, 41, 123, 125};
+
+	len = sizeof(seperators) / sizeof(seperators[0]);
+
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (ii = 0; ii < len; ii++)
+		{
+			if (seperators[ii] == str[i])
+				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+				{
+					str[i + 1] -= 32;
+				}
+		}
+	}
+
+	return (str);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,4 +9,5 @@ int _strlen(char *s);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *str);
+char *cap_string(char *str);
 #endif /*MAIN_H*/
