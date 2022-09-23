9976f3e1bb283b8c8a1cb2b835e2d991c7314b5c
cloobTech <belkid98@gmail.com>
2022-09-23 08:17:06 -0700

    print

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,18 +8,23 @@
 
 void print_number(int n)
 {
+	unsigned int k;
+
 	if (n < 0)
 	{
-	_putchar('-');
-	n = -n;
+	putchar('-');
+	k = -n;
 	}
+	else
+		k = n;
 
 
-	if (n >= 10)
+	if (k >= 10)
 	{
-		print_number(n / 10);
+		print_number(k / 10);
 
 	}
 
-	_putchar((n % 10) + '0');
+	putchar((k % 10) + '0');
 }
+
