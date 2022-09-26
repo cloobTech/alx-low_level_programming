f712c5142ad068bcf67972c047c87f57f418e14d
cloobTech <belkid98@gmail.com>
2022-09-26 06:26:07 -0700

    fixed

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,14 +12,13 @@
 
 char *_strchr(char *s, char c)
 {
-	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (*s != '\0')
 	{
-		if (s[i] == c)
-			return (&s[i]);
+		if (*s == c)
+			return (s);
+		s++;
 	}
-
 	return ('\0');
 
 }
