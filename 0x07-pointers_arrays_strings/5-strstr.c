8ae9729be22112dd6bfb7890f4637e252dbf37dd
cloobTech <belkid98@gmail.com>
2022-09-26 19:53:30 -0700

    chess

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strstr -  locates a substring.
+ *
+ * @haystack: string to be searched
+ * @needle: substring
+ *
+ * Return: a pointer to the beginning of the located substring
+ * or NULL if the substring is not found.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (needle[j] == haystack[i])
+			{
+				
+			}
+
+		}
+
+	}
+
+	return ('\0');
+}
