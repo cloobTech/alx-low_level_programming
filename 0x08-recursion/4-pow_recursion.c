c91855a8d63eec343c13e91d045d2073565018ff
cloobTech <belkid98@gmail.com>
2022-09-29 09:32:18 -0700

    pow

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -11,9 +11,10 @@
 
 int _pow_recursion(int x, int y)
 {
-
-	if (y < 1)
-		return (1);
+	if (y < 0)
+		return (-1);
+	if (y == 1)
+		return (x);
 	y--;
 	_pow_recursion(x, y);
 	return (_pow_recursion(x, y) * x);
