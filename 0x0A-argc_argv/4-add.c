7ca8e977d3cb1f565667bfd7e3a8ce3578923b31
cloobTech <belkid98@gmail.com>
2022-09-30 12:20:16 -0700

    task

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main -  a program that adds positive numbers.
+ *
+ * @argc: array counter
+ * @argv: array content
+ *
+ * Return: sum of integers in @argv
+ */
+
+
+int main(int argc, char *argv[])
+{
+	int i, sum;
+
+	sum = 0;
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (atoi(argv[i]) == 0)
+				continue;
+			if (atoi(argv[i]) > 0)
+			sum += atoi(argv[i]);
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("0\n");
+	}
+
+	return (0);
+}
