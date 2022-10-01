57f210518c1a270768de06d076700f3b96485c85
cloobTech <belkid98@gmail.com>
2022-10-01 11:10:23 -0700

    task

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,37 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
+#include <ctype.h>
 /**
- * main - prints the minimum number of coins to make change for an
- * amount of money.
- *
- * @argc: array counter
- * @argv: array content
- *
- * Return: Always (0) Success;
+ * main - is a function that prints the minimum number of coins for change
+ * @argc: is the number of arguments
+ * @argv: is the array of arguments
+ * Return: 0
  */
-
 int main(int argc, char *argv[])
 {
-	int change;
-	int coin[] = {1, 2, 5, 10, 25};
+	int value, c;
 
-
-	if (argc == 2)
-	{
-		if (atoi(argv[1]) < 0)
-		{
-			printf("0\n");
-		}
-		else
-		{
-		
-		}
-	}
-	else
+	c = 0;
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	value = atoi(argv[1]);
+	if (value < 0)
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
+	if (value % 25 >= 0)
+	{
+		c += value / 25;
+		value = value % 25;
+	}
+	if (value % 10 >= 0)
+	{
+		c += value / 10;
+		value = value % 10;
+	}
+	if (value % 5 >= 0)
+	{
+		c += value / 5;
+		value = value % 5;
+	}
+	if (value % 2 >= 0)
+	{
+		c += value / 2;
+		value = value % 2;
+	}
+	if (value % 1 >= 0)
+	{
+		c += value / 1;
+	}
+	printf("%d\n", c);
 	return (0);
 }
