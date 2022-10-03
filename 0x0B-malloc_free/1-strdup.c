d2de5ea02d41bf79a59f9ed55eb7d7941ab9c329
cloobTech <belkid98@gmail.com>
2022-10-03 02:18:18 -0700

    duplicate

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * _strdup -  returns a pointer to a newly allocated space in memory, which
@@ -10,13 +11,32 @@
  * Return: NULL if str = NULL || insufficient space
  * On success, the return a pointer to the duplicated string
  */
+/**
+ * main - check the code .
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *s;
 
+	s = _strdup(NULL);
+	if (s == NULL)
+	{
+		printf("failed to allocate memory\n");
+		return (1);
+	}
+	free(s);
+	return (0);
+}
 
 char *_strdup(char *str)
 {
 	char *dup_str;
 	unsigned int i, len;
 
+	if (str == NULL)
+		return (NULL);
 	i = 0;
 	len = 0;
 
@@ -36,11 +56,7 @@ char *_strdup(char *str)
 	i = 0;
 
 
-	if (str == NULL)
-		return (NULL);
-	if (dup_str == 0)
-		return (NULL);
-	if (str == 0)
+	if (dup_str == NULL)
 		return (NULL);
 	while (i < len)
 	{
