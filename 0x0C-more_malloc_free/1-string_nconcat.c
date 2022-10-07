73a7e206cb0ea39fef63fe4e485bd4deb74bdc7b
cloobTech <belkid98@gmail.com>
2022-10-07 01:24:53 -0700

    concat_str

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,61 @@
+#include <stdlib.h>
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * string_nconcat - concatenates two strings. only crop @n number of bytes
+ * of @s2.
+ *
+ * @s1: first string
+ * @s2: second string
+ * @n: n number of bytes of second string
+ *
+ * Return: pointer to a newly allocated space in memory,
+ * which contains s1, followed by the first n bytes of s2,
+ * and null terminated .If the function fails, it should return NULL
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *concat_str;
+	int len, s1_len, s2_len, i, int_n;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	s1_len = _strlen(s1);
+	s2_len = _strlen(s2);
+	int_n = n;
+	if (int_n >= s2_len)
+		s2_len = s2_len;
+	else
+		s2_len = int_n;
+
+	len = s1_len + s2_len;
+	i = 0;
+	/**
+	  * allocate dynamic memory using the string len
+	  */
+	concat_str = (char *) malloc((len * sizeof(*s1)) + 1);
+	if (concat_str == NULL)
+		return (NULL);
+	while (i < s1_len)
+	{
+		concat_str[i] = s1[i];
+		i++;
+	}
+	i = 0;
+	/**
+	 * concatenante from the last stop of S1;
+	 */
+	while (s1_len < len)
+	{
+		concat_str[s1_len] = s2[i];
+		i++;
+		s1_len++;
+	}
+	concat_str[len] = '\0';
+	return (concat_str);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 int _putchar(char c);
+int _strlen(char *s);
 void *malloc_checked(unsigned int b);
-
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 #endif /*MAIN_H*/
