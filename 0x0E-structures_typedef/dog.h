ee20ab852a965f36afb4ffde0a20ed77c0522a2f
cloobTech <belkid98@gmail.com>
2022-10-10 06:45:42 -0700

    structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - is a structure for a dog object
+ * @name: is the name element
+ * @age: is the age element
+ * @owner: is the owner element
+ */
+
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
