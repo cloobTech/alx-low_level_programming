e8f4691219baf5d313490d67a2ba49530aa026bf
cloobTech <belkid98@gmail.com>
2022-10-10 04:01:40 -0700

    preprocessoressor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the name of the file it was compiled from.
+ *
+ * Return: Always 0 (Success).
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+
+}
