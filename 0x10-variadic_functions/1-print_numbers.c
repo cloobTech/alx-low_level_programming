05207b0884bf098337540628cbcb090c18873177
cloobTech <belkid98@gmail.com>
2022-10-13 04:55:40 -0700

    variadic

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -27,7 +27,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 			if (i < (n - 1) && (separator != NULL))
 				printf("%s", separator);
 		}
-		printf("\n");
 		va_end(list);
+		printf("\n");
 	}
 }
