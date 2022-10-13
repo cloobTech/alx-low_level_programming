5d45086f48f2845ffc858702a3249cd6338cf9a8
cloobTech <belkid98@gmail.com>
2022-10-12 21:22:16 -0700

    x

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -6,4 +6,5 @@ int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void _puts_recursion(char *s);
 void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 #endif /*FUNCTION_POINTER*/
