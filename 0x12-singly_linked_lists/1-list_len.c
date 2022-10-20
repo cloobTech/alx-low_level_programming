c9a5bc1ba2107578c4583d649232f65880d1c409
cloobTech <belkid98@gmail.com>
2022-10-20 06:24:51 -0700

    task

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -8,7 +8,7 @@
  * Return: the number of nodes.
  */
 
- size_t list_len(const list_t *h)
+size_t list_len(const list_t *h)
 {
 	size_t count;
 
---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * add_node - add a new node at the beginning of a list_t list.
+ *
+ * @head: first node;
+ * @str: string (node data)
+ *
+ * Return: the address of the new element, or NULL if it failed;
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+
+
+
+}
