382a0a206940a794a218f19c17be9e55642b0a41
cloobTech <belkid98@gmail.com>
2022-10-20 20:53:31 -0700

    free

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list -  a function that frees a list_t list;
+ *
+ * @head: node head;
+ *
+ * Return: Nothing
+ */
+
+void free_list(list_t *head)
+{
+
+	while (head)
+	{
+		free(head);
+	}
+
+}
