e0ed12f1a188865263dd7c5214fb227c7c4e3ed6
cloobTech <belkid98@gmail.com>
2022-10-21 11:13:38 -0700

    task

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -33,12 +33,6 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 	new->str = strdup(str);
-	if (new->str == NULL)
-	{
-		free(new->str);
-		free(head);
-		return (NULL);
-	}
 	new->len = _strlen(str);
 	new->next = *head;
 	*head = new;
