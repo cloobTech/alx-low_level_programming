8ecd66e3c69f1e03714c9270b27114089f656846
cloobTech <belkid98@gmail.com>
2022-10-22 07:51:49 -0700

    assembly

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,15 +1,13 @@
+extern _printf
+global _main
+
 section .text
-	global _start
 
 _start:
-	mov edx, len
-	mov ecx, msg
-	mov ebx, 1
-	mov eax, 4
-	int 0x80
-
-	mov eax, 1
-	int 0x080
+_main :
+	push msg
+	call _printf
+	ret
 
 section .data
 	msg db "Hello, Holberton", 0xa
