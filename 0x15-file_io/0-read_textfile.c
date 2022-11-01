82bce3ba122955bfbf904364a1be1b6c35ac7eef
cloobTech <belkid98@gmail.com>
2022-11-01 00:18:35 -0700

    task

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -26,7 +26,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fd < 0)
 		return (0);
 	size_r = read(fd, buffer, letters);
-	if (size_r < 0)
+	if (size_r < 0 || len > size_r)
 		return (0);
 	size_w = write(1, buffer, letters);
 	free(buffer);
