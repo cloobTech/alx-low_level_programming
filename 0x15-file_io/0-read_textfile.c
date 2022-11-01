447d361d59d4d6f89adc7b19157178ef25c59faa
cloobTech <belkid98@gmail.com>
2022-11-01 05:47:44 -0700

    task

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -33,7 +33,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		letters = size_r;
 		len = letters;
 	}
-		size_w = write(STDOUT_FILENO, buffer, letters);
+		size_w = write(1, buffer, letters);
 	free(buffer);
 	if (size_w < 0 || size_w < len)
 		return (0);
