54877aded1824cd9a8af509a8cb9af8058224bed
cloobTech <belkid98@gmail.com>
2022-12-13 22:28:30 -0800

    fixed

---
+++ 0x18-dynamic_libraries/5-strstr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strstr -  locates a substring.
+ *
+ * @haystack: string to be searched
+ * @needle: substring
+ *
+ * Return: a pointer to the beginning of the located substring
+ * or NULL if the substring is not found.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j, len;
+
+	len = _strlen(needle);
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+			if (needle[j] != haystack[i + j])
+				break;
+		if (j == len)
+		{
+			return (&haystack[i]);
+		}
+	}
+	return ('\0');
+}
